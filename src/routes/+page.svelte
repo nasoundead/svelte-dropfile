8e05b5f6c33f223dd6bbb33ba5daccd697ab6c66
nasoundead <nasoundead@163.com>
2024-07-07 18:42:58 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Button from "./Button.svelte";
+  import Dropzone from "svelte-file-dropzone";
+
+  let files = {
+    accepted: [],
+    rejected: [],
+  };
+
+  function handleFilesSelect(e) {
+    const { acceptedFiles, fileRejections } = e.detail;
+    files.accepted = [...files.accepted, ...acceptedFiles];
+    files.rejected = [...files.rejected, ...fileRejections];
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Button />
+
+<Dropzone on:drop={handleFilesSelect} />
+<ol>
+  {#each files.accepted as item}
+    <li>{item.name}</li>
+  {/each}
+</ol>
